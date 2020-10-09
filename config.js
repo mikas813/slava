7298c07df80a48106d90d6a08e3dc0d018f39108
Mykhailo Lasiichuk <mishalasiychyk@MacBook-Air-Misha.local>
2020-10-09 17:55:49 +0100

    ch mondodb

--- config.js
+++ config.js
@@ -2,3 +2,8 @@ export default {
   MONGODB_URL: process.env.MONGODB_URL,
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
+
+const dbuser = 'user'
+const dbpassword = 'admin13'
+
+export const dbUrl = `mongodb://${dbuser}:${dbpassword}@ds155414.mlab.com:55414/heroku_9x2skcg0`
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import userRoute from './routes/userRoute.js'
 import uploadRoute from './routes/uploadRoute.js'
 import {createRequire} from 'module'
 import bodyParser from 'body-parser'
+import {dbUrl} from './config.js'
 const require = createRequire( import.meta.url )
 const path = require( 'path' )
 const __dirname = path.resolve()
@@ -14,8 +15,10 @@ const MongoDbUri = config.MONGODB_URL
 dotenv.config()
 
 const app = express()
+
 const PORT = process.env.PORT || 5000
-mongoose.connect( MongoDbUri || 'mongodb://localhost/slp', {
+
+mongoose.connect( process.env.MONGODB_URL || dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
@@ -25,6 +28,9 @@ mongoose.connect( MongoDbUri || 'mongodb://localhost/slp', {
 mongoose.connection.on('connected', () => {
   console.log('Mongose is connected!', )
 })
+mongoose.connection.on('error', err => {
+  console.log('Mongoose Default Connection Error : ' + err)
+})
 
 app.use( bodyParser.json() )
 app.use( '/api/uploads', uploadRoute )
@@ -32,11 +38,11 @@ app.use( '/api/users', userRoute )
 app.use( '/api/images', imageRoute )
 app.use( '/uploads', express.static( path.join( __dirname, '/uploads' ) ) )
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'development') {
   app.use( express.static( 'client/build' ) )
   app.get( '*', (req, res) => {
     res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html' ) )
   } )
 }
 
-app.listen(process.env.PORT || PORT, console.log( `Server started on port http://localhost:${PORT}, ${process.env.NODE_ENV}` ))
+app.listen(PORT, console.log( `Server started on port http://localhost:${PORT}, 'NODE_ENV = '${process.env.NODE_ENV}` ))
