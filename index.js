198b113510cac1cb1f4404562f1e59b2c03dd551
Mykhailo Lasiichuk <mishalasiychyk@MacBook-Air-Misha.local>
2020-10-09 17:59:53 +0100

    ch mondodb

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use( '/api/users', userRoute )
 app.use( '/api/images', imageRoute )
 app.use( '/uploads', express.static( path.join( __dirname, '/uploads' ) ) )
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   app.use( express.static( 'client/build' ) )
   app.get( '*', (req, res) => {
     res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html' ) )
